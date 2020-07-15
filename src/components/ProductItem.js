85234a86ef24cf6332d76efb160f01edc24eb522
vikttu <nespolopeixoto@outlook.com>
2020-07-15 09:20:04 -0300

    fixed the issue with the rendering, was a small bug and now it displays the error mensage when the api returns 500. I have decided to keep ProductItem component for better organization

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,54 +1,16 @@
 import React from 'react'
-//import axios from 'axios'
 import shoppingBag from '../assets/shopping-bag.svg'
 
-/*const productPlaceholder = {
-    id: 1,
-    name: 'Product',
-    price: '123.00',
-    quantity: 2,
-}*/
 
 function ProductItem(props){
     return(
         <div className="product-item">
             <img src={shoppingBag} alt="Product" height="75" width="150"/>
             <h4>{props.name}</h4>
-            <p>$ {props.price} - {props.quantity} left</p>
+            <p>$ {props.price} - {props.available} left</p>
             <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
         </div>
     )
 }
 
 export default ProductItem
-
-
-/*
-    export default class ProductItem extends React.Component {
-        state = {
-            products: [],
-        }
-    
-        componentDidMount() {
-            axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
-                .then(res => {
-                    console.log(res)
-                    this.setState({ products: res.data })
-                })
-        }
-    
-        render() {
-            return(
-                <div className="product-item">
-                <img src={shoppingBag} alt="Product" height="75" width="150"/>
-                {this.state.products.map(products => 
-                <div>
-                    <h4>{products.name}</h4>
-                    <p>$ {products.price} - {products.available} left</p>
-                </div>) }
-                <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
-                </div>            
-            )
-        }
-    }
-*/
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,30 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
-import shoppingBag from '../assets/shopping-bag.svg'
-//import ProductItem from './ProductItem'
-//import productsArray from './products-array'
+import ProductItem from './ProductItem'
 
-/*This component creates the products in a more dynamic way fetching external data*/
-/*
-function ProductsList(){
-    
-    const productsComponents = productsArray.map(product => <ProductItem 
-        key={product.id} 
-        name={product.name} 
-        price={product.price} 
-        quantity={product.quantity} />)
-        
-        return(
-            <div className="products-list">
-            {productsComponents}
-            </div>
-            )
-        }
-        
-        */
-
-        
 class ProductsList extends React.Component {
     
     constructor() {
@@ -35,27 +13,28 @@ class ProductsList extends React.Component {
 
     }
     
-    componentDidMount() {
-        const self = this; 
+    componentDidMount() { 
         axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
         .then(res => (
-            self.setState({ products: res.data.products })
-                ).catch((error) => self.setState({error: "Service not working. Try again later"})) )
+            this.setState({ products: res.data.products })))
+            .catch((error) => this.setState({error: "Service not working. Try again later"}))
     }
     
     render() {
+        if (this.state.error){
+            return(
+                <h1>Service not working. Try again later</h1>
+            )
+        }
         return(
             <div className="products-list">
             {
                 this.state.products.map(products => 
-                    <div>
-                        <div className="product-item">
-                        <img src={shoppingBag} alt="Product" height="75" width="150"/>
-                            <h4>{products.name}</h4>
-                            <p>$ {products.price} - {products.available} left</p>
-                            <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
-                        </div>            
-                    </div>) 
+                    <ProductItem 
+                    key={products.id} 
+                    name={products.name} 
+                    price={products.price} 
+                    available={products.available}  />) 
             }
             </div>
         )
