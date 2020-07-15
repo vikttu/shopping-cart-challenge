c0cb4e50c95b197d3dcb9303e672e033ba706e88
vikttu <nespolopeixoto@outlook.com>
2020-07-15 08:15:45 -0300

    changed ProductItem and ProductList functional components to a single class component inside ProductsList for better performance but it still has bugs, so I'll fix this issue later

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+//import axios from 'axios'
 import shoppingBag from '../assets/shopping-bag.svg'
 
 /*const productPlaceholder = {
@@ -20,3 +21,34 @@ function ProductItem(props){
 }
 
 export default ProductItem
+
+
+/*
+    export default class ProductItem extends React.Component {
+        state = {
+            products: [],
+        }
+    
+        componentDidMount() {
+            axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
+                .then(res => {
+                    console.log(res)
+                    this.setState({ products: res.data })
+                })
+        }
+    
+        render() {
+            return(
+                <div className="product-item">
+                <img src={shoppingBag} alt="Product" height="75" width="150"/>
+                {this.state.products.map(products => 
+                <div>
+                    <h4>{products.name}</h4>
+                    <p>$ {products.price} - {products.available} left</p>
+                </div>) }
+                <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
+                </div>            
+            )
+        }
+    }
+*/
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,23 +1,64 @@
 import React from 'react'
+import axios from 'axios'
 
-import ProductItem from './ProductItem'
-import productsArray from './products-array'
+import shoppingBag from '../assets/shopping-bag.svg'
+//import ProductItem from './ProductItem'
+//import productsArray from './products-array'
 
 /*This component creates the products in a more dynamic way fetching external data*/
-
+/*
 function ProductsList(){
-
+    
     const productsComponents = productsArray.map(product => <ProductItem 
         key={product.id} 
         name={product.name} 
         price={product.price} 
         quantity={product.quantity} />)
-
+        
         return(
-        <div className="products-list">
+            <div className="products-list">
             {productsComponents}
-        </div>
-    )
-}
+            </div>
+            )
+        }
+        
+        */
 
+        
+class ProductsList extends React.Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            products: [],
+        }
+
+    }
+    
+    componentDidMount() {
+        const self = this; 
+        axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
+        .then(res => (
+            self.setState({ products: res.data.products })
+                ).catch((error) => self.setState({error: "Service not working. Try again later"})) )
+    }
+    
+    render() {
+        return(
+            <div className="products-list">
+            {
+                this.state.products.map(products => 
+                    <div>
+                        <div className="product-item">
+                        <img src={shoppingBag} alt="Product" height="75" width="150"/>
+                            <h4>{products.name}</h4>
+                            <p>$ {products.price} - {products.available} left</p>
+                            <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
+                        </div>            
+                    </div>) 
+            }
+            </div>
+        )
+    }
+}
 export default ProductsList
