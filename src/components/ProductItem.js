f2d17447cd72493f0fa8f676e0b32148fc6a43d1
vikttu <nespolopeixoto@outlook.com>
2020-07-16 15:29:03 -0300

    I needed to convert the components back to functional ones instead of using class components. This took a long time and now I see there was no need to do it. I will try to use redux to fix the problem in passing data through sibiling components

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,56 +1,25 @@
 import React from 'react'
+import { setProducts } from './ShoppingBody'
 import shoppingBag from '../assets/shopping-bag.svg'
 
-let disabledState = false
 
-class ProductItem extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-            product: props
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick(){
-        if (this.state.product.available === 0){
-            return (disabledState = true)
-        }
-        this.setState(prevState =>{
-            return {
-                product: {
-                    name: prevState.product.name,
-                    price: prevState.product.price,
-                    available: prevState.product.available - 1
-                }
-            }
-        })
-    }
-
-    render(){
-        return(
-            <div className="product-item" >
-                <img src={shoppingBag} alt="Product" height="75" width="150"/>
-                <h4>{this.state.product.name}</h4>
-                <p>$ {this.state.product.price} - {this.state.product.available} (kg) left</p>
-                <button type="submit" className="buy-item" onClick={this.handleClick} disabled={disabledState}>BUY</button>
-            </div>
-        )
-    }
-} 
-
-/*
 function ProductItem(props){
+    let disabledState = false
+
+    function buyItem(){
+        setProducts(prevProducts => prevProducts.available -1)
+    }
+
+
     return(
         <div className="product-item">
         <img src={shoppingBag} alt="Product" height="75" width="150"/>
         <h4>{props.name}</h4>
         <p>$ {props.price} - {props.available} left</p>
-        <button type="submit" className="buy-item" onClick={() => console.log('buy item working!')} >BUY</button>
+        <button type="submit" className="buy-item" onClick={() => buyItem()} disabled={disabledState}>BUY</button>
         </div>
         )
     }
     
-*/
 
 export default ProductItem
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,44 +1,20 @@
 import React from 'react'
-import axios from 'axios'
 
 import ProductItem from './ProductItem'
 
-class ProductsList extends React.Component {
-    
-    constructor() {
-        super()
-        this.state = {
-            products: [],
-        }
 
-    }
-    
-    componentDidMount() { 
-        axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
-        .then(res => (
-            this.setState({ products: res.data.products })))
-            .catch((error) => this.setState({error: "Service not working. Try again later"}))
-    }
-    
-    render() {
-        if (this.state.error){
-            return(
-                <h1>Service not working. Try again later </h1>
-            )
+function ProductsList(props){
+    console.log(props)
+    const productsComponents = props.products.map(product => <ProductItem 
+        key={product.id} 
+        name={product.name} 
+        price={product.price} 
+        available={product.available} />)
+        console.log(productsComponents)
+        return( 
+        <div className="products-list">
+            {productsComponents}
+        </div>)
+
         }
-        return(
-            <div className="products-list">
-            {
-                this.state.products.map(products => 
-                    <ProductItem 
-                    key={products.id} 
-                    id={products.id}
-                    name={products.name} 
-                    price={products.price} 
-                    available={products.available}  />) 
-            }
-            </div>
-        )
-    }
-}
 export default ProductsList
